9affa3cbec898327b191fdcda40a7d895394a8cc
Vitorluzzz <luzvitor01@gmail.com>
2024-04-25 21:06:11 -0300

    Projeto Balanca

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,6 +7,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        Produtos produto = new Produtos();
+        produto.setName("Galinho");
+        produto.setPrice(11.50);
 
         System.out.println("Insert Tare: ");
         Scanner sc = new Scanner(System.in);
@@ -17,12 +22,19 @@ public class Main {
         if (codigoDePesagem == 120) {
             code = "Passarinho";
         }
+        /*try{
+            if(codigoDePesagem == )
+        }
+       // if(codigoDePesagem != ) */
         sc.close();
 
+
+        Date date = new Date();
         System.out.println("----------------------------------");
+        System.out.println("Date: " +date);
         System.out.println("-" + tare);
-        System.out.println("Produto: " + code);
-        System.out.println("Peso em KG");
+        System.out.println("Produto: " + produto.getName());
+        System.out.println("Peso em KG:" + produto.getPrice());
         System.out.println("Preço");
         System.out.println("-------------------------------------");
     }
--- src/Produtos.java
+++ src/Produtos.java
@@ -3,9 +3,33 @@ public class Produtos {
     public int codigoDePesagem;
     public double price;
 
-    public Produtos(String name, int codigoDePesagem, double price) {
+    public Produtos() {
         this.name = name;
         this.codigoDePesagem = codigoDePesagem;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCodigoDePesagem() {
+        return codigoDePesagem;
+    }
+
+    public void setCodigoDePesagem(int codigoDePesagem) {
+        this.codigoDePesagem = codigoDePesagem;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 }
